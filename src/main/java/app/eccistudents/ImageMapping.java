29f45566595436f7ae25113cf33e8b3586800532
nellson <nellsoncedeno@gmail.com>
2019-10-23 22:48:36 -0600

    ImageMapping code check

--- src/main/java/app/eccistudents/ImageMapping.java
+++ src/main/java/app/eccistudents/ImageMapping.java
@@ -1,18 +1,27 @@
 package app.eccistudents;
 
 public class ImageMapping{
-
-    public static final int[] dirR = {0,1,1,1,0,-1,-1,-1};
-    public static final int[] dirC = {-1,-1,0,1,1,1,0,-1};
     
+    public static final int[] DIR_R = {-1,-1,0,1,1,1,0,-1};
+    public static final int[] DIR_C = {0,1,1,1,0,-1,-1,-1};
+
+    public static final boolean ORIGINAL_MATRIX = true;
+    public static final boolean POSITIVE_DIRECTION = true;
+    public static final boolean VERTICAL_DIRECTION = true;
+    
+    private Catalog catalog;
+
     int[][] matrix;
     int[][] componentsMatrix;
+
     int[][] tempMatrix;
     int[][] tempComponentsMatrix;
 
 	int backgroundColor;
+    int backgroundTag;
+
     int borderColor;
-    int borderIndex;
+    int borderTag;
 
     int insideColor;
     int spotsNumber;
@@ -22,13 +31,7 @@ public class ImageMapping{
 
     int rowsLength;
     int columnsLength;
-    
-    int backgroundTag;
-    
-    int matrixValue;
-    int matrixNextValue;
-    int counter = 0;
-    
+        
     Imagen image;
 
     public ImageMapping(String imageName){
@@ -40,7 +43,7 @@ public class ImageMapping{
 
         backgroundColor = matrix[0][0];
         backgroundTag = -1;
-        borderIndex = -1;
+        borderTag = -1;
 
         newPosC = 0;
         newPosR = 0;
@@ -54,85 +57,52 @@ public class ImageMapping{
 
     public void mapImage(){
         mapBackground(0,0);
-        while(borderIndex < backgroundTag){
+        
+        int figures = -1 * (borderTag + 1);
+        
+        catalog = new Catalog(figures);
+        while(borderTag < backgroundTag){
             delimitFigure();
-            ++borderIndex;
+            ++borderTag;
         }
         Imagen image2 = new Imagen(componentsMatrix);
         image2.dibujar();
     }
-    
-    public void mapBackground(int posR, int posC){
-        componentsMatrix[posR][posC] = backgroundTag;
-        
-        for(int i=0;i<dirC.length;i+=2){
-            newPosC = posC + dirC[i];
-            newPosR = posR + dirR[i];
-            if(positionInBounds(newPosR,newPosC)){
-                if(componentsMatrix[newPosR][newPosC] == 0){
-                    if(matrix[newPosR][newPosC] == backgroundColor){
-                        mapBackground(newPosR,newPosC);
-                    } else {
-                        borderIndex--;
-                        borderColor = matrix[newPosR][newPosC];
-                        mapBorder(newPosR,newPosC);
-                    }
-                }
-            }   
-        }
-    }
-    
-    public void mapBorder(int posR, int posC){
-        componentsMatrix[posR][posC] = borderIndex;
-        
-        for(int i=0;i<dirC.length;++i){
-            newPosC = posC + dirC[i];
-            newPosR = posR + dirR[i];
-            if(positionInBounds(newPosR,newPosC)){
-                if(matrix[newPosR][newPosC] == borderColor && componentsMatrix[newPosR][newPosC] == 0){
-                    mapBorder(newPosR,newPosC);
-                } 
-            }
-        }
-    }
-    
+
     public void delimitFigure(){
 
-        spotsNumber = 0;
+        int additionalPixels = 3;        
+        int newPixels = 1;
 
-        int[] topMatch = new int[2];    // No tienen que ser vectores
-        int[] bottomMatch = new int[2];
-        int[] rightMatch = new int[2];
-        int[] leftMatch = new int[2];
+        int bottomMatch = 0;
+        int rightMatch = 0;
+        int leftMatch = 0;
+        int topMatch = 0;
 
         int height;
         int width;
+        
+        spotsNumber = 0;
 
-        topMatch = searchVertical(true);
-        bottomMatch = searchVertical(false);
-        leftMatch = searchHorizontal(true);
-        rightMatch = searchHorizontal(false);
+        topMatch = searchMatch( POSITIVE_DIRECTION , VERTICAL_DIRECTION);
+        bottomMatch = searchMatch( !POSITIVE_DIRECTION , VERTICAL_DIRECTION);
+        leftMatch = searchMatch( POSITIVE_DIRECTION , !VERTICAL_DIRECTION);
+        rightMatch = searchMatch( !POSITIVE_DIRECTION , !VERTICAL_DIRECTION);
 
-        height = bottomMatch[0] - topMatch[0] + 2;
-        width = rightMatch[1] - leftMatch[1] + 2;
+        height = bottomMatch - topMatch + additionalPixels;
+        width = rightMatch - leftMatch + additionalPixels;
 
         tempMatrix = new int[height][width];
         tempComponentsMatrix = new int[height][width];
 
         int rows = 0;
         int columns;
-        for(int r = (topMatch[0] - 1); r <= bottomMatch[0]; ++r){
+        
+        for(int r = (topMatch - newPixels); r <= (bottomMatch + newPixels); ++r){
             columns = 0;
-            for(int c = (leftMatch[1] - 1); c <= rightMatch[1]; ++c){
+        
+            for(int c = (leftMatch - newPixels); c <= (rightMatch + newPixels); ++c){
                 tempMatrix[rows][columns] = matrix[r][c];
-                ++columns;
-            } 
-            ++rows;
-        }   
-        rows = 0;
-        for(int r = (topMatch[0] - 1); r <= bottomMatch[0]; ++r){
-            columns = 0;
-            for(int c = (leftMatch[1] - 1); c <= rightMatch[1]; ++c){
                 tempComponentsMatrix[rows][columns] = componentsMatrix[r][c];
                 ++columns;
             } 
@@ -140,47 +110,75 @@ public class ImageMapping{
         }   
         
         cleanFigure();
-        constructImages();
         findInside();
-        System.out.println(spotsNumber);
+        constructFigure();
     }
-
-    public void constructImages(){
-        Imagen imageColors = new Imagen(tempMatrix);
-        imageColors.dibujar();
-        Imagen imageFigure = new Imagen(tempComponentsMatrix);
-        imageFigure.dibujar();    
-    }
-
-    public void cleanFigure(){
-        for(int r = 0; r < tempMatrix.length; ++r){
-            for(int c = 0; c < tempMatrix[r].length; ++c){
-                if(tempComponentsMatrix[r][c] != backgroundTag && tempComponentsMatrix[r][c] != borderIndex && tempComponentsMatrix[r][c] != 0){
-                    cleanBorder(r,c);
+    
+    public void mapBackground(int posR, int posC){
+        componentsMatrix[posR][posC] = backgroundTag;
+        
+        for(int i=0;i<DIR_C.length;i+=2){
+            newPosC = posC + DIR_C[i];
+            newPosR = posR + DIR_R[i];
+        
+            if(positionInBounds(newPosR,newPosC,ORIGINAL_MATRIX)){
+        
+                if(componentsMatrix[newPosR][newPosC] == 0){
+        
+                    if(matrix[newPosR][newPosC] == backgroundColor){
+                        mapBackground(newPosR,newPosC);
+                    } else {
+                        borderTag--;
+                        borderColor = matrix[newPosR][newPosC];
+                        mapBorder(newPosR,newPosC,true);
+                    }
                 }
-            }
+            }   
         }
     }
 
-    public void cleanBorder(int posR, int posC){
-        tempComponentsMatrix[posR][posC] = backgroundTag;
-        tempMatrix[posR][posC] = backgroundColor;
+    public void mapBorder(int posR, int posC, boolean add){
+        
+        if(add){
+            componentsMatrix[posR][posC] = borderTag;
+        } else {
+            tempComponentsMatrix[posR][posC] = backgroundTag;
+            tempMatrix[posR][posC] = backgroundColor;    
+        }
 
-        for(int i=0;i<dirC.length;++i){
-            newPosC = posC + dirC[i];
-            newPosR = posR + dirR[i];
-            if(positionInBoundsFigure(newPosR,newPosC)){
-                if(tempComponentsMatrix[newPosR][newPosC] != backgroundTag){
-                    cleanBorder(newPosR,newPosC);
+        for(int i=0;i<DIR_C.length;++i){
+            newPosC = posC + DIR_C[i];
+            newPosR = posR + DIR_R[i];
+        
+            if(positionInBounds(newPosR,newPosC,add)){
+        
+                if(add ? addBorder() : cleanBorder()){
+                    mapBorder(newPosR,newPosC,add);
                 } 
             }
         }
     }
 
+    public void cleanFigure(){
+   
+        for(int r = 0; r < tempMatrix.length; ++r){
+   
+            for(int c = 0; c < tempMatrix[r].length; ++c){
+   
+                if(borderToClean(r,c)){
+                    mapBorder(r,c,false);
+                }
+            }
+        }
+    }
+    
     public void findInside(){
         boolean found = false;
+        
         for(int r = 0; r < tempMatrix.length && !found; ++r){
+            
             for(int c = 0; c < tempMatrix[r].length && !found; ++c){
+                
                 if(tempComponentsMatrix[r][c] == 0){
                     insideColor = tempMatrix[r][c];
                     searchSpots(r,c);
@@ -189,18 +187,21 @@ public class ImageMapping{
             }
         }
     }
-
+    
     public void searchSpots(int posR, int posC){
         tempComponentsMatrix[posR][posC]= 1;
-
-        for(int i=0;i<dirC.length;i+=2){
-            newPosC = posC + dirC[i];
-            newPosR = posR + dirR[i];
-            if(positionInBoundsFigure(newPosR,newPosC) && tempComponentsMatrix[newPosR][newPosC] == 0){
+        
+        for(int i=0;i<DIR_C.length;i+=2){
+            newPosC = posC + DIR_C[i];
+            newPosR = posR + DIR_R[i];
+            
+            if(positionInBounds(newPosR,newPosC, !ORIGINAL_MATRIX) && tempComponentsMatrix[newPosR][newPosC] == 0){
+                
                 if(tempMatrix[newPosR][newPosC] != insideColor){
                     ++spotsNumber;
                     mapSpots(newPosR, newPosC);
                 } else {
+                
                     if(tempMatrix[newPosR][newPosC] == insideColor){
                         searchSpots(newPosR, newPosC);
                     }
@@ -212,10 +213,12 @@ public class ImageMapping{
     public void mapSpots(int posR, int posC){
         tempComponentsMatrix[posR][posC] = 1;
         
-        for(int i=0;i<dirC.length;++i){
-            newPosC = posC + dirC[i];
-            newPosR = posR + dirR[i];
-            if(positionInBoundsFigure(newPosR,newPosC)){
+        for(int i=0;i<DIR_C.length;++i){
+            newPosC = posC + DIR_C[i];
+            newPosR = posR + DIR_R[i];
+
+            if(positionInBounds(newPosR,newPosC, !ORIGINAL_MATRIX)){
+                
                 if(tempMatrix[newPosR][newPosC] != insideColor && tempComponentsMatrix[newPosR][newPosC] == 0){
                     mapSpots(newPosR,newPosC);
                 } 
@@ -223,29 +226,46 @@ public class ImageMapping{
         }
     }
     
-    public int[] searchVertical(boolean top){
+    public int searchMatch(boolean positiveDir, boolean verticalDir){
+        
         boolean found;
-        int[] address = new int[2];
-        int startingRow = 1;
-        int startingColumn = 1;
-        int endRow = 1;
+
         int addNumber = 1;
-        if(top){
-            endRow = rowsLength;
-            address[0] = componentsMatrix.length-1;
-            address[1] = componentsMatrix[0].length-1;
+        int address = 0;
+        int start1 = 1;
+        int start2 = 1;
+        int end1 = 1;
+        int end2 = 1;
+        
+        if(verticalDir){
+            end1 = columnsLength;
+
+            if(positiveDir){
+                end2 = rowsLength;
+                address = rowsLength - 1;
+            } else {
+                start2 = rowsLength - 1;
+                addNumber = -1;
+            }
         } else {
-            startingRow = rowsLength - 1;
-            addNumber = -1;
+            end1 = rowsLength;
+
+            if(positiveDir){
+                end2 = columnsLength;
+                address = columnsLength -1; 
+            } else {
+                start2 = columnsLength -1;
+                addNumber = -1;
+            }
         }
-        for(int c = startingColumn; c < columnsLength; ++c){
+        for(int i = start1; i < end1; ++i){
             found = false;
-            for(int r = startingRow;top?(r<endRow):(r>endRow) && !found;r+=addNumber){
-                if(componentsMatrix[r][c] == borderIndex){
+
+            for(int j = start2;positiveDir?(j<end2):(j>end2) && !found;j+=addNumber){
+                if(verticalDir?(componentsMatrix[j][i] == borderTag):(componentsMatrix[i][j] == borderTag)){
                     found = !found;
-                    if(top?(r < address[0]):(r > address[0])){
-                        address[0] = r;
-                        address[1] = c;
+                    if(positiveDir?(j < address):(j > address)){
+                        address = j;
                     }
                 }
             }
@@ -253,46 +273,31 @@ public class ImageMapping{
         return address;
     }
     
-    public int[] searchHorizontal(boolean left){
-        boolean found;
-        int[] address = new int[2];
-        int startingRow = 1;
-        int startingColumn = 1;
-        int endColumn = 1;
-        int addNumber = 1;
-        if(left){
-            endColumn = columnsLength;
-            address[0] = componentsMatrix.length-1;
-            address[1] = componentsMatrix[0].length-1;
+    public void constructFigure(){
+        Imagen imageColors = new Imagen(tempMatrix);
+        Imagen imageFigure = new Imagen(tempComponentsMatrix);
+    }
+
+    public boolean cleanBorder(){
+        return (tempComponentsMatrix[newPosR][newPosC] != backgroundTag);
+    }
+
+    public boolean addBorder(){
+        return (matrix[newPosR][newPosC] == borderColor && componentsMatrix[newPosR][newPosC] == 0);
+    }
+
+    public boolean borderToClean(int r, int c){
+        return (tempComponentsMatrix[r][c] != backgroundTag && tempComponentsMatrix[r][c] != borderTag && tempComponentsMatrix[r][c] != 0);
+    }
+    
+    public boolean positionInBounds(int r, int c, boolean imageMatrix){
+        boolean inBounds = false;
+        
+        if(imageMatrix){
+            inBounds = (r < matrix.length) && (c < matrix[0].length) && (r >= 0) && (c >= 0); 
         } else {
-            startingColumn = columnsLength - 1;
-            addNumber = -1;
+            inBounds = (r < tempMatrix.length) && (c < tempMatrix[0].length) && (r >= 0) && (c >= 0); 
         }
-        for(int r = startingRow; r < rowsLength; ++r){
-            found = false;
-            for(int c = startingColumn;left?(c<endColumn):(c>endColumn) && !found;c+=addNumber){
-                if(componentsMatrix[r][c] == borderIndex){
-                    found = !found;
-                    if(left?(c < address[1]):(c > address[1])){
-                        address[0] = r;
-                        address[1] = c;
-                    }
-                }
-            }
-        }
-        return address;
+        return inBounds;
     }
-    
-    
-    
-    public boolean positionInBounds(int r, int c){
-        return (r < matrix.length) && (c < matrix[0].length) && (r >= 0) && (c >= 0);
-    }
-    
-    public boolean positionInBoundsFigure(int r, int c){
-        return (r < tempMatrix.length) && (c < tempMatrix[0].length) && (r >= 0) && (c >= 0);
-    }
-
-
-
 }
